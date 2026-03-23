5412cfc4fa1c80f4eb912d479f0014b4728de36a
julianlamaravilla <julianrestrepo012@gmail.com>
2026-03-23 09:15:21 -0500

    second version and add comments

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
+// Necessary library
+import javax.swing.*;
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main{
+
+    // Principal function
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
         int option1 = 0;
         int option2 = 0;
         double real = 0.0;
 
-
+        // try to capture possible error
         try {
-            System.out.println("Please Select first temperature\n1. Celsius\n2. Fahrenheit\n3. Kelvin\n");
-            option1 = scanner.nextInt();
-            System.out.println("Now the amount : \n");
-            real = scanner.nextDouble();
-            System.out.println("Please Select the unit to convert:\n1. Celsius\n2. Fahrenheit\n3. Kelvin\n");
-            option2 = scanner.nextInt();
 
+            // input the variables values
+            option1 = Integer.parseInt(JOptionPane.showInputDialog("Please Select first temperature\n1. Celsius\n2. Fahrenheit\n3. Kelvin\n"));
+            real = Double.parseDouble(JOptionPane.showInputDialog("Now the amount : \n"));
+            option2 = Integer.parseInt(JOptionPane.showInputDialog("Please Select the unit to convert:\n1. Celsius\n2. Fahrenheit\n3. Kelvin\n"));
+
+            // capture a possible exception when option1 is equal to option2
             if(option1 == option2) {
-                System.out.println("Result : " + real);
+                JOptionPane.showMessageDialog(null, "Result : " + real);
                 System.exit(0);
+
+            // When option1 or option2 is out of the range, capture the exception
             } else if (option1 > 3 || option2 > 3 || option1 < 1 || option2 < 1) {
-                System.out.print("Error,please, select correct values in range");
+                JOptionPane.showMessageDialog(null, "Error,please, select correct values in range");
                 main(args);
                 System.exit(0);
             }
 
-        } catch (InputMismatchException e) {
-            System.out.print("Error: " + e + ", please, select correct values");
+        // Capture the exception
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e + ", please, select correct values");
             main(args);
             System.exit(0);
         }
@@ -36,22 +41,28 @@ public class Main{
         // logic
         if (option1 == 1 && option2 == 2 ){ // Celsius to Fahrenheit
             double result = (real * 1.8) + 32;
-            System.out.println("Celsius : "+ real + "\nFahrenheit : " + result);
+            JOptionPane.showMessageDialog(null, "Celsius : "+ real + "\nFahrenheit : " + result);
+
         } else if (option1 == 1 && option2 == 3){ // Celsius to Kevin
             double result = (real + 273.15);
-            System.out.println("Celsius : "+ real + "\nKelvin : " + result);
+            JOptionPane.showMessageDialog(null,"Celsius : "+ real + "\nKelvin : " + result);
+
         }else if (option1 == 2 && option2 == 1) { // Fahrenheit to Celsius
             double result = (real - 32)/1.8;
-            System.out.println("Fahrenheit : "+ real + "\nCelsius : " + result);
+            JOptionPane.showMessageDialog(null, "Fahrenheit : "+ real + "\nCelsius : " + result);
+
         } else if (option1 == 2 && option2 == 3) { // Fahrenheit to kelvin
             double result = (((real - 32)/1.8) + 273.15);
-            System.out.println("Fahrenheit : "+ real + "\nKelvin : " + result);
+            JOptionPane.showMessageDialog(null,"Fahrenheit : "+ real + "\nKelvin : " + result );
+
         } else if (option1 == 3 && option2 == 1) { // Kevin to Celsius
             double result = (real - 273.15);
-            System.out.println("Kevin : "+ real + "\nCelsius : " + result);
+            JOptionPane.showMessageDialog(null,"Kevin : "+ real + "\nCelsius : " + result );
+
         } else if (option1 == 3 && option2 == 2) { // Kelvin to Fahrenheit
             double result = ((1.8)*(real - 273.15))+ 32;
-            System.out.println("Kevin : "+ real + "\nFahrenheit : " + result);
+            JOptionPane.showMessageDialog(null,"Kevin : "+ real + "\nFahrenheit : " + result );
+
         } else {
             System.out.print("Error");
         }
